b45d5ad32ca508930881a3e22ec4e846dce9d4a7
pranshulpradhan <139995434+pranshulpradhan@users.noreply.github.com>
2023-10-02 19:42:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//Get a reference to the div element using its ID
+var divElement = document.getElementById("div1");
+
+// Change the styles of the div element
+divElement.style.color = "blue";            // Change text color to blue
+divElement.style.backgroundColor = "yellow"; // Change background color to yellow
+divElement.style.margin = "20px";           // Add a margin of 20px
+divElement.style.padding = "10px";          // Add a padding of 10px
+divElement.style.fontSize = "18px";         // Change font size to 18px
+divElement.style.fontWeight = "bold";       // Set font weight to bold
+divElement.style.height = "200px";          // Change height to 200px
+divElement.style.width = "300px";           // Change width to 300px
